f3eb8389f328d83b4a89bb8e4fffe7d3e43778a9
Alfiya9023 <alfiyanazar9961713989@gmail.com>
2024-06-24 10:57:38 +0530

    Add files via upload

---
+++ proj4.py
@@ -0,0 +1,8 @@
+class Group(object):
+    def addTwoNumbers(self,first,second):
+        self.first=first
+        self.second=second
+    @staticmethod   
+    def addz(a,b):
+        print(a+b)
+Group.addz(12,2)
